8b52ce060a7d8f04d6c5e88da915b5a835f1168e
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-05-27 22:13:34 +0300

    Add AssetBundleLoader

---
+++ Packages/UGF.Module.AssetBundles/Runtime/AssetBundleLoadParameters.cs
@@ -0,0 +1,9 @@
+﻿using UGF.Module.Assets.Runtime;
+
+namespace UGF.Module.AssetBundles.Runtime
+{
+    public class AssetBundleLoadParameters : IAssetLoadParameters
+    {
+        public static AssetBundleLoadParameters Default { get; } = new AssetBundleLoadParameters();
+    }
+}
