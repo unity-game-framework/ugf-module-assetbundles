e602c1344a5bcb70dfcf0932b6b628bcba3e1e88
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-05-27 22:38:57 +0300

    Add AssetBundleLoaderFile

---
+++ Packages/UGF.Module.AssetBundles/Runtime/AssetBundleFileInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace UGF.Module.AssetBundles.Runtime
+{
+    public class AssetBundleFileInfo : AssetBundleInfo
+    {
+        public uint Crc { get; set; }
+        public ulong Offset { get; set; }
+
+        public AssetBundleFileInfo(string loaderId, string address) : base(loaderId, address)
+        {
+        }
+    }
+}
---
+++ Packages/UGF.Module.AssetBundles/Runtime/AssetBundleLoaderFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using UGF.RuntimeTools.Runtime.Contexts;
+using UnityEngine;
+
+namespace UGF.Module.AssetBundles.Runtime
+{
+    public class AssetBundleLoaderFile : AssetBundleLoader<AssetBundleFileInfo, AssetBundleLoadParameters, AssetBundleUnloadParameters>
+    {
+        protected override AssetBundle OnLoadAssetBundle(AssetBundleFileInfo info, string id, Type type, AssetBundleLoadParameters parameters, IContext context)
+        {
+            AssetBundle assetBundle = AssetBundle.LoadFromFile(info.Address, info.Crc, info.Offset);
+
+            return assetBundle;
+        }
+
+        protected override async Task<AssetBundle> OnLoadAssetBundleAsync(AssetBundleFileInfo info, string id, Type type, AssetBundleLoadParameters parameters, IContext context)
+        {
+            AssetBundleCreateRequest operation = AssetBundle.LoadFromFileAsync(info.Address, info.Crc, info.Offset);
+
+            while (!operation.isDone)
+            {
+                await Task.Yield();
+            }
+
+            AssetBundle assetBundle = operation.assetBundle;
+
+            return assetBundle;
+        }
+    }
+}
