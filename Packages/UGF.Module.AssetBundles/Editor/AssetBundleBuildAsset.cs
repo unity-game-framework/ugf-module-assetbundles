aa560ae17d1694e6add524cf81a9f53721ab433d
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-06-01 22:55:12 +0300

    Add AssetBundleBuildAsset

---
+++ Packages/UGF.Module.AssetBundles/Editor/AssetBundleBuildAsset.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UGF.EditorTools.Runtime.IMGUI.AssetReferences;
+using UGF.Module.AssetBundles.Runtime;
+using UnityEngine;
+
+namespace UGF.Module.AssetBundles.Editor
+{
+    [CreateAssetMenu(menuName = "Unity Game Framework/Assets/Asset Bundle Build", order = 2000)]
+    public class AssetBundleBuildAsset : ScriptableObject
+    {
+        [SerializeField] private List<AssetReference<AssetBundleAsset>> m_assetBundles = new List<AssetReference<AssetBundleAsset>>();
+
+        public List<AssetReference<AssetBundleAsset>> AssetBundles { get { return m_assetBundles; } }
+    }
+}
---
+++ Packages/UGF.Module.AssetBundles/Editor/AssetBundleBuildEditorUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UGF.Module.AssetBundles.Runtime;
+
+namespace UGF.Module.AssetBundles.Editor
+{
+    public static class AssetBundleBuildEditorUtility
+    {
+        public static void Build(AssetBundleBuildAsset buildAsset)
+        {
+            if (buildAsset == null) throw new ArgumentNullException(nameof(buildAsset));
+        }
+
+        public static void Refresh(AssetBundleBuildAsset buildAsset)
+        {
+            if (buildAsset == null) throw new ArgumentNullException(nameof(buildAsset));
+        }
+
+        public static void GetGroupsAll(Dictionary<string, AssetBundleAsset> assetBundles)
+        {
+        }
+    }
+}
