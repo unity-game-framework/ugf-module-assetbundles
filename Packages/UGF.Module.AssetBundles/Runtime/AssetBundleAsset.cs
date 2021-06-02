de213e62a55989f5d2287a4862bb3e9c88fe0e0e
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-06-02 21:36:21 +0300

    Change AssetBundleAsset

--- Packages/UGF.Module.AssetBundles/Runtime/AssetBundleAsset.cs
+++ Packages/UGF.Module.AssetBundles/Runtime/AssetBundleAsset.cs
@@ -10,10 +10,12 @@ namespace UGF.Module.AssetBundles.Runtime
     {
         [AssetGuid(typeof(AssetLoaderAsset))]
         [SerializeField] private string m_loader;
+        [SerializeField] private uint m_crc;
         [AssetGuid(typeof(AssetBundleAsset))]
         [SerializeField] private List<string> m_dependencies = new List<string>();
 
         public string Loader { get { return m_loader; } set { m_loader = value; } }
+        public uint Crc { get { return m_crc; } set { m_crc = value; } }
         public List<string> Dependencies { get { return m_dependencies; } }
     }
 }
--- Packages/UGF.Module.AssetBundles/Runtime/AssetBundleFileAsset.cs
+++ Packages/UGF.Module.AssetBundles/Runtime/AssetBundleFileAsset.cs
@@ -8,18 +8,16 @@ namespace UGF.Module.AssetBundles.Runtime
     {
         [AssetGuid(typeof(AssetBundleStorageAsset))]
         [SerializeField] private string m_storage;
-        [SerializeField] private uint m_crc;
         [SerializeField] private ulong m_offset;
 
         public string Storage { get { return m_storage; } set { m_storage = value; } }
-        public uint Crc { get { return m_crc; } set { m_crc = value; } }
         public ulong Offset { get { return m_offset; } set { m_offset = value; } }
 
         protected override IAssetBundleInfo OnBuild()
         {
             var info = new AssetBundleFileInfo(Loader, m_storage)
             {
-                Crc = m_crc,
+                Crc = Crc,
                 Offset = m_offset
             };
 
