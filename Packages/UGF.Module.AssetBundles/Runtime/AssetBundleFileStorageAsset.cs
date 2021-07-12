3783fa6f0c199d2fe265c8a2307962282211f09c
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-07-12 23:03:32 +0300

    Remove AssetBundleFileStorageUtility and AssetBundleFileStorageDirectory

--- Packages/UGF.Module.AssetBundles/Runtime/AssetBundleFileStorageAsset.cs
+++ Packages/UGF.Module.AssetBundles/Runtime/AssetBundleFileStorageAsset.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UGF.RuntimeTools.Runtime.Storage;
 using UnityEngine;
 
 namespace UGF.Module.AssetBundles.Runtime
@@ -6,15 +7,15 @@ namespace UGF.Module.AssetBundles.Runtime
     [CreateAssetMenu(menuName = "Unity Game Framework/Assets/Asset Bundle File Storage", order = 2000)]
     public class AssetBundleFileStorageAsset : AssetBundleStorageAsset
     {
-        [SerializeField] private AssetBundleFileStorageDirectory m_directory = AssetBundleFileStorageDirectory.None;
+        [SerializeField] private StoragePathType m_directory;
         [SerializeField] private string m_directoryPath;
 
-        public AssetBundleFileStorageDirectory Directory { get { return m_directory; } set { m_directory = value; } }
+        public StoragePathType Directory { get { return m_directory; } set { m_directory = value; } }
         public string DirectoryPath { get { return m_directoryPath; } set { m_directoryPath = value; } }
 
         protected override IAssetBundleStorage OnBuild()
         {
-            string path = AssetBundleFileStorageUtility.GetPath(m_directory, m_directoryPath);
+            string path = StorageUtility.GetPath(m_directory, m_directoryPath);
             var storage = new AssetBundleFileStorage(path);
 
             return storage;
