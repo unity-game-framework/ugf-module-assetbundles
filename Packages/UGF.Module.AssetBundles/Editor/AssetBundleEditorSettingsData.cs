64b6a4e10d12af3a1674cd44107477ae7ae69c61
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-26 22:04:06 +0300

    Change AssetBundleEditorSettings

--- Packages/UGF.Module.AssetBundles/Editor/AssetBundleEditorSettingsData.cs
+++ Packages/UGF.Module.AssetBundles/Editor/AssetBundleEditorSettingsData.cs
@@ -6,8 +6,10 @@ namespace UGF.Module.AssetBundles.Editor
 {
     public class AssetBundleEditorSettingsData : CustomSettingsData
     {
+        [SerializeField] private bool m_buildBeforeEnterPlaymode;
         [SerializeField] private List<AssetBundleBuildAsset> m_builds = new List<AssetBundleBuildAsset>();
 
+        public bool BuildBeforeEnterPlaymode { get { return m_buildBeforeEnterPlaymode; } set { m_buildBeforeEnterPlaymode = value; } }
         public List<AssetBundleBuildAsset> Builds { get { return m_builds; } }
     }
 }
