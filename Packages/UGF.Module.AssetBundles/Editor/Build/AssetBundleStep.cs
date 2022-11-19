70af2d1db30dd4aaa95bf21afc3b41e284023bca
Vorobey <vorobyovvorobyov@gmail.com>
2022-11-19 22:13:25 +0100

    Add AssetBundleStep

---
+++ Packages/UGF.Module.AssetBundles/Editor/Build/AssetBundleStep.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UGF.Build.Editor;
+using UGF.Logs.Runtime;
+using UGF.RuntimeTools.Runtime.Contexts;
+
+namespace UGF.Module.AssetBundles.Editor.Build
+{
+    public class AssetBundleStep : BuildStep
+    {
+        public IReadOnlyList<AssetBundleBuildAsset> Builds { get; }
+
+        public AssetBundleStep(IReadOnlyList<AssetBundleBuildAsset> builds)
+        {
+            Builds = builds ?? throw new ArgumentNullException(nameof(builds));
+        }
+
+        protected override void OnExecute(IBuildSetup setup, IContext context)
+        {
+            AssetBundleBuildEditorUtility.BuildAll(Builds);
+
+            Log.Info("Asset bundles built", new { Builds.Count });
+        }
+    }
+}
---
+++ Packages/UGF.Module.AssetBundles/Editor/Build/AssetBundleStepAsset.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UGF.Build.Editor;
+using UnityEngine;
+
+namespace UGF.Module.AssetBundles.Editor.Build
+{
+    [CreateAssetMenu(menuName = "Unity Game Framework/Assets/Asset Bundle Step", order = 2000)]
+    public class AssetBundleStepAsset : BuildStepAsset
+    {
+        [SerializeField] private List<AssetBundleBuildAsset> m_builds = new List<AssetBundleBuildAsset>();
+
+        public List<AssetBundleBuildAsset> Builds { get { return m_builds; } }
+
+        protected override IBuildStep OnBuild()
+        {
+            return new AssetBundleStep(m_builds);
+        }
+    }
+}
