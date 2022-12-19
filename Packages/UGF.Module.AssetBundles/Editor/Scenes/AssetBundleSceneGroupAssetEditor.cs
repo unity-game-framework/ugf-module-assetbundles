1f38e86efb4057392866d8e907b172f13cb92334
Vorobey <vorobyovvorobyov@gmail.com>
2022-12-19 18:48:15 +0100

    Add AssetBundleSceneGroupAsset

---
+++ Packages/UGF.Module.AssetBundles/Editor/Scenes/AssetBundleSceneGroupAssetEditor.cs
@@ -0,0 +1,74 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UGF.Module.AssetBundles.Runtime.Scenes;
+using UGF.Module.Scenes.Editor;
+using UGF.Module.Scenes.Editor.Loaders.Manager;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Module.AssetBundles.Editor.Scenes
+{
+    [CustomEditor(typeof(AssetBundleSceneGroupAsset), true)]
+    internal class AssetBundleSceneGroupAssetEditor : UnityEditor.Editor
+    {
+        private SerializedProperty m_propertyLoader;
+        private SerializedProperty m_propertyAssetBundle;
+        private ReorderableListDrawer m_listScenes;
+
+        private void OnEnable()
+        {
+            m_propertyLoader = serializedObject.FindProperty("m_loader");
+            m_propertyAssetBundle = serializedObject.FindProperty("m_assetBundle");
+
+            m_listScenes = new ReorderableListDrawer(serializedObject.FindProperty("m_scenes"))
+            {
+                DisplayAsSingleLine = true
+            };
+
+            m_listScenes.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listScenes.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                EditorGUILayout.PropertyField(m_propertyLoader);
+                EditorGUILayout.PropertyField(m_propertyAssetBundle);
+
+                m_listScenes.DrawGUILayout();
+            }
+
+            EditorGUILayout.Space();
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.FlexibleSpace();
+
+                if (GUILayout.Button("Refresh All", GUILayout.Width(75F)))
+                {
+                    ManagerSceneEditorUtility.UpdateSceneGroupAll();
+                }
+
+                if (GUILayout.Button("Refresh", GUILayout.Width(75F)))
+                {
+                    SceneReferenceEditorUtility.UpdateScenePathAll(m_listScenes.SerializedProperty);
+
+                    serializedObject.ApplyModifiedProperties();
+                }
+            }
+
+            if (!SceneReferenceEditorUtility.ValidateReferencesAll(m_listScenes.SerializedProperty))
+            {
+                EditorGUILayout.Space();
+                EditorGUILayout.HelpBox("Scene reference collection contains entries with missing or invalid data.", MessageType.Warning);
+            }
+        }
+    }
+}
---
+++ Packages/UGF.Module.AssetBundles/Runtime/Scenes/AssetBundleSceneGroupAsset.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UGF.EditorTools.Runtime.Assets;
+using UGF.EditorTools.Runtime.Ids;
+using UGF.Module.Scenes.Runtime;
+using UGF.Module.Scenes.Runtime.Loaders.Manager;
+using UnityEngine;
+
+namespace UGF.Module.AssetBundles.Runtime.Scenes
+{
+    [CreateAssetMenu(menuName = "Unity Game Framework/Assets/Asset Bundle Scene Group", order = 2000)]
+    public class AssetBundleSceneGroupAsset : ManagerSceneGroupAsset
+    {
+        [AssetId(typeof(AssetBundleAsset))]
+        [SerializeField] private GlobalId m_assetBundle;
+
+        public GlobalId AssetBundle { get { return m_assetBundle; } set { m_assetBundle = value; } }
+
+        protected override void OnGetScenes(IDictionary<GlobalId, ISceneInfo> scenes)
+        {
+            for (int i = 0; i < Scenes.Count; i++)
+            {
+                SceneReference reference = Scenes[i];
+
+                scenes.Add(reference.Guid, new AssetBundleSceneInfo(Loader, m_assetBundle, reference.Path));
+            }
+        }
+    }
+}
