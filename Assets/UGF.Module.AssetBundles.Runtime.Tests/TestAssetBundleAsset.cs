7bf3338d8e4b4e7d627438ca81f3f7cd343bbfcd
Vorobey <vorobyovvorobyov@gmail.com>
2023-01-04 22:51:59 +0100

    Add tests

---
+++ Assets/UGF.Module.AssetBundles.Runtime.Tests/TestAssetBundleAsset.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UGF.Module.AssetBundles.Runtime.Tests
+{
+    [CreateAssetMenu(menuName = "Tests/TestAssetBundleAsset")]
+    public class TestAssetBundleAsset : AssetBundleAsset
+    {
+        protected override IAssetBundleInfo OnBuild()
+        {
+            return null;
+        }
+    }
+}
