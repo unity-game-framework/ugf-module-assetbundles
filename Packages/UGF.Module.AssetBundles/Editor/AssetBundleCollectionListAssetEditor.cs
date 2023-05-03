93225ad1b1954a3e991de9b5a6ed009808c93017
Vorobey <vorobyovvorobyov@gmail.com>
2023-05-03 14:18:13 +0200

    Add AssetBundleCollectionListAsset

---
+++ Packages/UGF.Module.AssetBundles/Editor/AssetBundleCollectionListAssetEditor.cs
@@ -0,0 +1,45 @@
+﻿using UGF.EditorTools.Editor.Assets;
+using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UGF.Module.AssetBundles.Runtime;
+using UnityEditor;
+
+namespace UGF.Module.AssetBundles.Editor
+{
+    [CustomEditor(typeof(AssetBundleCollectionListAsset), true)]
+    internal class AssetBundleCollectionListAssetEditor : UnityEditor.Editor
+    {
+        private AssetIdReferenceListDrawer m_listAssetBundles;
+        private ReorderableListSelectionDrawerByPath m_listAssetBundlesSelection;
+
+        private void OnEnable()
+        {
+            m_listAssetBundles = new AssetIdReferenceListDrawer(serializedObject.FindProperty("m_assetBundles"));
+
+            m_listAssetBundlesSelection = new ReorderableListSelectionDrawerByPath(m_listAssetBundles, "m_asset")
+            {
+                Drawer = { DisplayTitlebar = true }
+            };
+
+            m_listAssetBundles.Enable();
+            m_listAssetBundlesSelection.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listAssetBundles.Disable();
+            m_listAssetBundlesSelection.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                m_listAssetBundles.DrawGUILayout();
+                m_listAssetBundlesSelection.DrawGUILayout();
+            }
+        }
+    }
+}
