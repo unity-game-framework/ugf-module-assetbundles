c07de29279bee4c65bc0685b7956758e8058ad4d
Vorobey <vorobyovvorobyov@gmail.com>
2023-11-17 16:34:22 +0300

    Add AssetBundleGroupFolderAsset

---
+++ Packages/UGF.Module.AssetBundles/Editor/AssetBundleGroupFolderAsset.cs
@@ -0,0 +1,37 @@
+﻿using UGF.EditorTools.Runtime.Ids;
+using UGF.Module.AssetBundles.Runtime;
+using UGF.Module.Assets.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Module.AssetBundles.Editor
+{
+    [CreateAssetMenu(menuName = "Unity Game Framework/Assets/Asset Bundle Group Folder", order = 2000)]
+    public class AssetBundleGroupFolderAsset : AssetFolderAsset
+    {
+        [SerializeField] private AssetBundleGroupAsset m_group;
+
+        public AssetBundleGroupAsset Group { get { return m_group; } set { m_group = value; } }
+
+        protected override bool OnIsValid()
+        {
+            return m_group != null;
+        }
+
+        protected override void OnUpdate()
+        {
+            m_group.Assets.Clear();
+
+            string[] guids = FindAssetsAsGuids();
+
+            for (int i = 0; i < guids.Length; i++)
+            {
+                string guid = guids[i];
+
+                m_group.Assets.Add(new GlobalId(guid));
+            }
+
+            EditorUtility.SetDirty(m_group);
+        }
+    }
+}
